5a1892e8723bba16a2d08ed30be2ee267441004c
liujunhua <483717867@qq.com>
2026-07-15 18:29:54 +0800

    fix railway port read env PORT

--- backend/app.py
+++ backend/app.py
@@ -536,4 +536,6 @@ def startup():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    import os
+    port = int(os.environ.get("PORT", 80))
+    uvicorn.run(app, host="0.0.0.0", port=port)
